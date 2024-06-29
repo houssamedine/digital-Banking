23b529e52e7c49f4d4d0ae50b70fbd607e4a2deb
houssamedine <houssamedineelazhari@gmail.com>
2024-06-29 14:33:46 +0100

    first commit

---
+++ src/test/java/com/e_bank/E_Banking/EBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_bank.E_Banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
